4555b994987d6f2f459a67fef333ce63f5061108
Lars Thise Pedersen <ltpe@techcollege.dk>
2025-05-22 14:23:18 +0200

    Add project files.

---
+++ ERP_System_Unit_Test/Address_UnitTest.cs
@@ -0,0 +1,48 @@
+﻿using ERP_System;
+using System.Diagnostics.Metrics;
+using Xunit;
+
+namespace ERP_System_Test
+{
+    public class Address_UnitTest
+    {
+        [Fact]
+        public void TestCase1()
+        {
+            // Arrange
+            string Street = "Struervej";
+            string City = "Aalborg";
+            string State = "Nordjylland";
+            string HouseNumber = "70";
+            string PostalCode = "9280";
+            string StreetNumber = "70";
+            string Country = "Danmark";
+
+            // Act
+            Address addressTestObject = new Address();
+
+            addressTestObject.Street = Street;
+            addressTestObject.City = City;
+            addressTestObject.State = State;
+            addressTestObject.HouseNumber = HouseNumber;
+            addressTestObject.PostalCode = PostalCode;
+            addressTestObject.StreetNumber = StreetNumber;
+            addressTestObject.Country = Country;
+
+            string addressString = addressTestObject.GetAddress();
+            
+            // Assert
+            string addressStringTrimmed = addressString.Trim();
+            string[] stringsInAddressString = addressStringTrimmed.Split(new char[] { ',' });
+
+            Assert.Equal(HouseNumber, stringsInAddressString[0]);
+            Assert.Equal(Street, stringsInAddressString[1]);
+            Assert.Equal(StreetNumber, stringsInAddressString[2]);
+            Assert.Equal(City, stringsInAddressString[3]);
+            Assert.Equal(State, stringsInAddressString[4]);
+            Assert.Equal(PostalCode, stringsInAddressString[5]);
+            Assert.Equal(Country, stringsInAddressString[6]);
+            //return $"{HouseNumber},{Street}, {StreetNumber}, {City}, {State}, {PostalCode}, {Country}";
+        }
+    }
+}
